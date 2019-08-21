65da83ad40fb366e2fd1ec627d077bc2c6f90c5b
Andrey Masiero <andreymasiero@gmail.com>
2019-08-20 23:53:23 -0300

    redirect sign up page added

--- WebContent/scripts/app.js
+++ WebContent/scripts/app.js
@@ -1,1 +1,6 @@
 var btnCadastrar = document.querySelector("#cadastrar");
+
+btnCadastrar.addEventListener("click", function(event) {
+	event.preventDefault();
+	window.location = "cadastro.html";
+});
